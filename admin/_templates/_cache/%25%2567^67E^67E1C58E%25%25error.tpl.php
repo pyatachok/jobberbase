c362fe620ca76e57104888f51e23ef73d21564ca
laptop1 <laptop1@laptop1.(none)>
2014-04-02 13:01:18 +0300

    first commit

---
+++ admin/_templates/_cache/%25%2567^67E^67E1C58E%25%25error.tpl.php
@@ -0,0 +1,22 @@
+<?php /* Smarty version 2.6.26, created on 2014-03-28 16:22:41
+         compiled from error.tpl */ ?>
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => "header.tpl", 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+
+		<div id="content">
+			<div id="job-listings"></div><!-- #job-listings -->
+			<div id="no-ads">
+				The page you were looking for was not found.<br />
+				Would you like to <a href="<?php echo $this->_tpl_vars['BASE_URL_ADMIN']; ?>
+home/">return to homepage?</a>?
+			</div>
+		</div><!-- /content -->
+
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => "footer.tpl", 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
